4ba4a7687c6febf69408777285d8814ea1df56f3
Atlas <alfaroasadiara@gmail.com>
2021-09-19 09:09:05 -0300

    upload project

--- config/config.js
+++ config/config.js
@@ -7,5 +7,6 @@ const config = {
         'mongodb://' + (process.env.IP || 'localhost') + ':' +
         (process.env.MONGO_PORT || '27017') +
         '/mernproject'
+    //mongodb+srv://admin:admin@cluster0.apawq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 }
 export default config 
