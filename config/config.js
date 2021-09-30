0f3c04df6a169ee63fe3510c1b1810a453b87d63
Atlas <alfaroasadiara@gmail.com>
2021-09-30 18:22:04 -0300

    Upgrade project

--- config/config.js
+++ config/config.js
@@ -3,9 +3,8 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI ||
-        process.env.MONGO_HOST ||
-        'mongodb://' + (process.env.IP || 'localhost') + ':' +
-        (process.env.MONGO_PORT || '27017') +
-        '/mernproject'
-}
-export default config 
+      process.env.MONGO_HOST ||
+      "mongodb+srv://admin:<password>.apawq.mongodb.net/<DB>?retryWrites=true&w=majority"
+  }
+  
+  export default config
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,7 +1,7 @@
 const path = require('path')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
-
+const nodeExternals = require('webpack-node-externals')
 const config = {
     name: "server",
     entry: [path.join(CURRENT_WORKING_DIR, './server/server.js')],
@@ -19,13 +19,8 @@ const config = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
-            },
-            {
-                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
-                use: 'file-loader'
             }
         ]
     }
 }
-
 module.exports = config
